1451eb99cf0a1f2fbc1b9959dd79a7c0fc527305
Soham Das <79685489+Mark-42-max@users.noreply.github.com>
2022-06-21 03:58:10 +0530

    Debugging pt.2

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,7 +3,7 @@ import {Container} from 'reactstrap'
 
 const Login = () => {
 
-    const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=831d4897ac7646da97fecad41eb430ef&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-private%20user-read-email%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20";
+    const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=831d4897ac7646da97fecad41eb430ef&response_type=code&redirect_uri=https://mark-42-max.github.io/Spotify_Clone/&scope=streaming%20user-read-private%20user-read-email%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20";
 
     return (
         <Container className="d-flex justify-content-center align-items-center" style={{minHeight:"100vh"}}>
